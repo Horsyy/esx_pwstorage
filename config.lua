62d0c6ce334c81dd836a9aa6bb473bc6ada2b944
Horse <42266290+Horsyy@users.noreply.github.com>
2021-06-29 19:56:10 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@ Config.MarkerType                 = 1
 Config.MarkerSize                 = {x = 1.5, y = 1.5, z = 0.5}
 Config.MarkerColor                = {r = 50, g = 50, b = 204}
 Config.Locale                     = 'en'
+Config.PasswordLength             = 4
 
 Config.EnableBlips                = true
 Config.BlipTransparency           = 255
