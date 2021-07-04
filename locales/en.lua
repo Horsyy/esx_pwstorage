d2dd1747ec8b2165d50b6d438fae18c443c897a7
Horse <42266290+Horsyy@users.noreply.github.com>
2021-07-04 17:16:50 +0300

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,7 +1,31 @@
 Locales['en'] = {
   ['open_storage'] = 'press ~INPUT_CONTEXT~ to access the ~y~Storage~s~.',
+
+	-- Commands Lang --
+  ['StorageHelp'] = 'By using this command you can create a new Storage.',
+  ['SocietyHelp'] = 'Society, for example /addStorage Horse',
+  ['SocietyHelpPass'] = 'Society Password, for example /addStorage Horse 1234',
+  ['SocietyExpired'] = 'Society Expiration Date, (year-month-date) for example /addStorage Horse 1234 2021-07-10',
+  ['renewHelpPass'] = 'Society Password, for example /renewStorage 1234',
+  ['renewExpired'] = 'Society Expiration Date, (year-month-date) for example /renewStorage 1234 2021-07-10',
+
+	-- Notifications Lang --
+  ['preparing'] = 'Preparing Storage for Society: ~o~society_%sStorage~s~ with Password: ~o~%s~s~',
+  ['expired'] = 'Your subscription ~r~expired~s~. Please contact us if you want to ~g~renew~s~ it.',
+  ['exists'] = 'The ~y~Society~s~ you are trying to create already ~r~exists~s~.',
+  ['existsPass'] = 'The ~y~Password~s~ you are trying to create already ~r~exists~s~.',
+  ['addedStorage'] = 'Added Storage for Society: ~g~%s~s~',
+  ['busy_msg'] = 'Storage is currently ~r~Busy',
+  ['canceling'] = '∑ ~r~Canceling..',
+
+	-- Menu Lang --
   ['menu_title'] = 'Password Storage',
   ['menu_pass'] = 'Type Password',
+
+	-- ProgressBar Lang --
+  ['addingdb'] = 'Adding DB - Press "X" or "ESC" to Cancel',
   ['progbar'] = 'Opening..',
+
+	-- Blip Name --
   ['blip'] = 'Storage'
 }
