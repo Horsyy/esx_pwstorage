cfd8fff711f9dde22c980d8a4e2fd3bb545092b9
Horse <42266290+Horsyy@users.noreply.github.com>
2021-07-05 19:23:26 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Horse#0001'
+description 'ESX Password Storage'
+version '1.0.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+dependencies {
+	'es_extended',
+	'esx_inventoryhud',
+	'mythic_progbar'
+}
